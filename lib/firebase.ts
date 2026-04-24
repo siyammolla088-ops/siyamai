cd4e64f2add9daee7a660fe2e7bfd3a04d738b15
siyammolla088-ops <siyammolla088@gmail.com>
2026-04-24 19:38:46 +0600

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,15 +1,14 @@
-// path: lib/firebase.ts
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAs-G5uK9hGv-S-qO_e8vXpM7_v6eG-W8", // আপনার স্ক্রিনশট থেকে নিলাম
+  apiKey: "AIzaSyAs-G5uK9hGv-S-qO_e8vXpM7_v6eG-W8", 
   authDomain: "siyamai-bd7c3.firebaseapp.com",
   projectId: "siyamai-bd7c3",
   storageBucket: "siyamai-bd7c3.firebasestorage.app",
   messagingSenderId: "542365451952",
-  appId: "1:542365451952:web:644a30349635b719069d6b" // আপনার স্ক্রিনশট থেকে নিলাম
+  appId: "1:542365451952:web:644a30349635b719069d6b"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
