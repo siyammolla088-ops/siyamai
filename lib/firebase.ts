3c1d910e160b0fed489d1913aaec6695e664396f
siyammolla088-ops <siyammolla088@gmail.com>
2026-04-24 22:26:23 +0600

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AlzaSyAhc5CCT8CsNCcaKhiBQ7IoAlOuUKtSibI", // আপনার অরিজিনাল কী
-  authDomain: "siyamai-5a94c.firebaseapp.com",
-  projectId: "siyamai-5a94c",
-  storageBucket: "siyamai-5a94c.firebasestorage.app",
-  messagingSenderId: "941063493380",
-  appId: "1:941063493380:web:6fccb30bb84e385abb2251",
-  measurementId: "G-30EHVDHGLT"
+  authDomain: "siyamai-5a94c.firebaseapp.com", //
+  projectId: "siyamai-5a94c", //
+  storageBucket: "siyamai-5a94c.firebasestorage.app", //
+  messagingSenderId: "941063493380", //
+  appId: "1:941063493380:web:6fccb30bb84e385abb2251", //
+  measurementId: "G-30EHVDHGLT" //
 };
 
 const app = initializeApp(firebaseConfig);
