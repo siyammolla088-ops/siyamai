8da1bace24bdeeeea264b993b6896952b5912643
siyammolla088-ops <siyammolla088@gmail.com>
2026-04-24 19:35:25 +0600

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,15 +4,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "আপনার_API_KEY_এখানে_দিন", // স্ক্রিনশট থেকে দিন (AIzaSy...)
+  apiKey: "AIzaSyAs-G5uK9hGv-S-qO_e8vXpM7_v6eG-W8", // আপনার স্ক্রিনশট থেকে নিলাম
   authDomain: "siyamai-bd7c3.firebaseapp.com",
   projectId: "siyamai-bd7c3",
   storageBucket: "siyamai-bd7c3.firebasestorage.app",
   messagingSenderId: "542365451952",
-  appId: "আপনার_APP_ID_এখানে_দিন" // স্ক্রিনশট থেকে দিন (1:542365...)
+  appId: "1:542365451952:web:644a30349635b719069d6b" // আপনার স্ক্রিনশট থেকে নিলাম
 };
 
-// Next.js এ বারবার রিলোড হলে যেন এরর না দেয় তাই এই চেকটি করা হয়
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
 export const auth = getAuth(app);
