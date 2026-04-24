6b7ad7f42c424263c2a590f909b05aea5ab78bef
siyammolla088-ops <siyammolla088@gmail.com>
2026-04-24 21:49:44 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        net: false,
+        tls: false,
+        fs: false,
+      };
+    }
+    return config;
+  },
+};
+
 export default nextConfig;
